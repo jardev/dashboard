1d3778d5a8c2a52ac6ffe98eb045e35951b2e138
jardev <jardev@gmail.com>
2011-01-07 00:58:38 +0200

    Upgraded verison in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dashboard "0.0.3"
+(defproject dashboard "0.0.4"
   :description "Dashboard"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
